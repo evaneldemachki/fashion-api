6f17adfeeb9b6c33fb77cd153774d45cb69a7b86
Evan <evaneldemachki@gmail.com>
2020-08-29 04:00:35 -0400

    removed /all route from API, set a default limit of 20

--- routes.js
+++ routes.js
@@ -14,28 +14,14 @@ module.exports = function(ctx) {
         }
         if(req.query.limit) {
             limit = parseInt(req.query.limit)
-            collection.find(mongo_query).limit(limit).toArray(function(err, result) {
-                if (err) throw err;
-                res.status(200).send(result);
-                next();
-            })
         } else {
-            collection.find(mongo_query).toArray(function(err, result) {
-                if (err) throw err;
-                res.status(200).send(result);
-                next();
-            })            
+            limit = 20
         }
 
-
-    })
-
-    server.get('/all', (req, res, next) => {
-        collection.find({}).toArray(function(err, result) {
+        collection.find(mongo_query).limit(limit).toArray(function(err, result) {
             if (err) throw err;
-            res.send(200, result)
-            next()
+            res.status(200).send(result);
+            next();
         })
-
     })
 }
