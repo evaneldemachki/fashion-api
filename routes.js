c8fa9a8532744f3910901cbfbc5ef05496756089
Evan <evaneldemachki@gmail.com>
2020-09-04 06:22:27 -0400

    added name parameter to API for text indexing name field

--- routes.js
+++ routes.js
@@ -3,69 +3,55 @@ module.exports = function(ctx) {
     const server = ctx.server;
     const collection = db.collection("Items");
     
-    const query_param = ["category", "gender", "limit"]
+    const query_param = ["category", "gender", "limit", "name", "devmode"]
 
-    server.get('/api/search', (req, res, next) => {
-        let mongo_query = {};
-        let limit;
-        
-        // send 400 if any query parameters are not in query_param
-        if(Object.keys(req.query).some((key) => !query_param.includes(key))) {
-            res.status(400).send('valid parameters: ["category", "gender", "limit"]');
-            next();
-        }
+    // return MongoDB query given request parameters
+    const constructQuery = function(req) {
+        let mongo_query = { };
+
         if(req.query.category) {
             mongo_query["category"] = req.query.category;
         }
         if(req.query.gender) {
             mongo_query["gender"] = req.query.gender;
         }
-        if(req.query.limit) {
-            limit = parseInt(req.query.limit)
-        } else {
-            limit = 20
+        if(req.query.name) {
+            mongo_query["$text"] = { "$search" : req.query.name };
         }
-
-        collection.find(mongo_query).limit(limit).toArray(function(err, result) {
-            if (err) throw err;
-            res.status(200).send(result);
-            next();
-        });
-    });
-    
-    // developer route: returns placeholder images for testing purposes
-    server.get('/api/search-dev', (req, res, next) => {
-        let mongo_query = {};
-        let limit;
         
+        return mongo_query;
+      };
+
+    server.get('/api/search', (req, res, next) => { 
         // send 400 if any query parameters are not in query_param
         if(Object.keys(req.query).some((key) => !query_param.includes(key))) {
-            res.status(400).send('valid parameters: ["category", "gender", "limit"]');
+            res.status(400).send("valid parameters: category, gender, limit, name");
             next();
         }
-        if(req.query.category) {
-            mongo_query["category"] = req.query.category;
-        }
-        if(req.query.gender) {
-            mongo_query["gender"] = req.query.gender;
-        }
+        // get request object from query constructor
+        let mongo_query = constructQuery(req);
+        let limit;
+
+        // use default limit if not included in request
         if(req.query.limit) {
-            limit = parseInt(req.query.limit)
+            limit = parseInt(req.query.limit);
         } else {
             limit = 20;
         }
-
+        // fetch response
         collection.find(mongo_query).limit(limit).toArray(function(err, result) {
             if (err) throw err;
-            for(key in result) {
-                for(img in result[key]["img"]) {
-                    result[key]["img"][img] = "http://fashionapi.herokuapp.com/whale.jpg"
+            // replace all images with local whale.jpg if devmode="true"
+            if(req.query.devmode == "true") {
+                for(key in result) {
+                    for(img in result[key]["img"]) {
+                        result[key]["img"][img] = "http://fashionapi.herokuapp.com/whale.jpg"
+                    }
                 }
             }
-            
+
             res.status(200).send(result);
             next();
         });
     });
-
 }
